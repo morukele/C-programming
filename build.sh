678e5d1a1aafd164fecbffd437eddb4455cc0551
Oghenemarho Orukele <orukele.dev@gmail.com>
2026-01-02 13:49:55 +0100

    implemented basic arena allocator

---
+++ build.sh
@@ -0,0 +1,3 @@
+set -xe
+
+gcc arena_allocator.c -o output/arena_allocator
--- pressure.c
+++ pressure.c
@@ -3,18 +3,9 @@
 
 int main()
 {
-    printf("Enter water pressure reading (mPa)\n");
+    int imageWidth = 1500;
+    int imageHeight = 1000;
 
-    double pressureMPa;
-    scanf("%lf", &pressureMPa);
-
-    double pressureBars = pressureMPa * 10;
-    if (fabs(pressureBars - 3.4) < 0.0001)
-    {
-        printf("Pipe has reached ideal pressure\n");
-    }
-    else
-    {
-        printf("Pipe is not at ideal pressure\n");
-    }
+    double ratio = (double)imageWidth / (double)imageHeight;
+    printf("Image Ratio: %lf\n", ratio);
 }
