10569145e13a18e907736d582d1d13c8c0ef7fe4
Oghenemarho Orukele <orukele.dev@gmail.com>
2026-02-28 11:51:29 +0100

    initialised c struct

--- struct.c
+++ struct.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <string.h>
-#include <time.h>
 
 struct UserProfile
 {
@@ -12,7 +10,7 @@ struct UserProfile
     uint8_t level;
 };
 
-void update_profile(struct UserProfile *p)
+void update_profile(struct UserProfile* p)
 {
     p->x_pos += 10;
     p->y_pos -= 5;
@@ -21,18 +19,28 @@ void update_profile(struct UserProfile *p)
 
 struct TimerRegister
 {
-    uint32_t CONTROL;   // Offset 0x00
-    uint32_t RELOAD;    // Offset 0x04
-    uint32_t VALUE;     // Offset 0x08
+    uint32_t CONTROL; // Offset 0x00
+    uint32_t RELOAD; // Offset 0x04
+    uint32_t VALUE; // Offset 0x08
     uint32_t INTERRUPT; // Offset 0x0C
 };
 
 void init_timer()
 {
-    volatile struct TimerRegister *timer = (struct TimerRegister *)0x40001000;
+    volatile struct TimerRegister* timer = (struct TimerRegister*)0x40001000;
 
     timer->RELOAD = 0xFFFF;
     timer->CONTROL |= 0x01;
 }
 
-int main() {}
+int main()
+{
+    struct UserProfile userProfile = {
+        .username = {'O', 'g', 'h', 'e', 'n', 'e', 'm', 'a', 'r', 'h', 'o'},
+        .id = 100,
+        .x_pos = 0,
+        .y_pos = 0,
+        .level = 0
+    };
+    update_profile(&userProfile);
+}
